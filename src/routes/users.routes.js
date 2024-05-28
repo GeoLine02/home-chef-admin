4f91c40fbfa7435893e15bc400bcd8b4f69f1a6c
Nika Wuladze <126957977+GeoLine02@users.noreply.github.com>
2024-05-28 13:06:06 +0400

    add: create userRouts file

---
+++ src/routes/users.routes.js
@@ -0,0 +1,10 @@
+const express = require("express");
+const router = express.Router();
+const userController = require("../controllers/user.controller");
+
+router.get("/", userController.getAllUsers);
+router.get("/:id", userController.getUserByID);
+router.delete("/delete", userController.deleteUserByID);
+router.put("/update", userController.updateUserByID);
+
+module.exports = router;
