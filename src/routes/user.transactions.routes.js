08f56e6a087038f3e7d8738b863fbcb80b193cdb
Nika Wuladze <126957977+GeoLine02@users.noreply.github.com>
2024-05-29 17:59:39 +0400

    add: create getTransactionById route

--- src/routes/user.transactions.routes.js
+++ src/routes/user.transactions.routes.js
@@ -3,5 +3,6 @@ const router = express.Router();
 const userTransactionsController = require("../controllers/user.transactions.controller");
 
 router.get("/", userTransactionsController.getAllTransactions);
+router.get("/:id", userTransactionsController.getTransactionById);
 
 module.exports = router;
