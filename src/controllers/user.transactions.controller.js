e8c7e94264febd1c5715c68d83b16fbec27a30a4
Nika Wuladze <126957977+GeoLine02@users.noreply.github.com>
2024-05-29 17:58:52 +0400

    add: create getTransactionByID controller fucntion

--- src/controllers/user.transactions.controller.js
+++ src/controllers/user.transactions.controller.js
@@ -13,6 +13,22 @@ const getAllTransactions = async (req, res) => {
   }
 };
 
+const getTransactionById = async (req, res) => {
+  try {
+    const { id } = req.body;
+    const transactionById =
+      await userTransactionsService.getTransactionById(id);
+    res.status(200).json(transactionById);
+  } catch (error) {
+    if (res.status(404)) {
+      res.json({ message: `transaction with ${id} does not exists` });
+    } else if (res.status(500)) {
+      res.json({ message: "internal server error" });
+    }
+  }
+};
+
 module.exports = {
   getAllTransactions,
+  getTransactionById,
 };
