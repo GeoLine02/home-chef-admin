02f7364034f8fa1e77d7157ccd8386d7ccaad929
Nika Wuladze <126957977+GeoLine02@users.noreply.github.com>
2024-05-31 16:18:00 +0400

    add: create delete restaurant types  jucniotions route

--- src/routes/restaurant.types.routes.js
+++ src/routes/restaurant.types.routes.js
@@ -4,5 +4,9 @@ const restaurantTypesController = require("../controllers/restaurant.types");
 
 router.get("/", restaurantTypesController.getAllRestaurantTypes);
 router.post("/choose", restaurantTypesController.ChooseRestaurantTypes);
+router.delete(
+  "/delete",
+  restaurantTypesController.deleteRestauarntTypesJucntions
+);
 
 module.exports = router;
