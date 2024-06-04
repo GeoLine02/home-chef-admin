d2dfc08b744706ba895954b580b97cbfc5079736
Nika Wuladze <126957977+GeoLine02@users.noreply.github.com>
2024-06-04 15:45:42 +0400

    add: create restaurant acitve hours update route

--- src/routes/restaurant.settings.routes.js
+++ src/routes/restaurant.settings.routes.js
@@ -2,7 +2,8 @@ const express = require("express");
 const router = express.Router();
 const restaurantSettingsController = require("../controllers/restaurant.settings.controller");
 
-router.post("/setWorkingHours", restaurantSettingsController.setActiveHours);
+router.post("/setActiveHours", restaurantSettingsController.setActiveHours);
+router.put("/update/activeHours", restaurantSettingsController.setActiveHours);
 router.post("/setWorkingDays", restaurantSettingsController.setWorkingDays);
 router.get("/", restaurantSettingsController.getWorkingDays);
 router.post("/setActiveHours", restaurantSettingsController.setActiveHours);
