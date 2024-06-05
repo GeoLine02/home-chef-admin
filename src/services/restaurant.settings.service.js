961e5302035094402be04cc3f54941909e22f475
Nika Wuladze <126957977+GeoLine02@users.noreply.github.com>
2024-06-05 23:43:16 +0400

    add: create updateActiveHours service function

--- src/services/restaurant.settings.service.js
+++ src/services/restaurant.settings.service.js
@@ -14,40 +14,24 @@ async function setActiveHours(restaurantID, workingFrom, workingTill) {
   }
 }
 
+async function updateActiveHours(restaurantID, workingFrom, workingTill) {
+  try {
+    const query = `UPDATE "RestaurantSettings" SET "workingFrom" = $2, workinTill = $3, WHERE "restaurantID" = $1`;
+    const res = await pool.query(query, [
+      restaurantID,
+      workingFrom,
+      workingTill,
+    ]);
+    return res.rows;
+  } catch (error) {
+    throw error;
+  }
+}
+
 async function deleteActiveHours(restaurantID) {
   try {
     const query = `DELETE FROM "RestaurantSettings" WHERE "restaurantID" = $1`;
     const res = await pool.query(query, [restaurantID]);
-    return res.rows;
-  } catch (error) {
-    throw error;
-  }
-}
-
-async function setWorkingDays(workingDays) {
-  try {
-    const query = `INSERT INTO "Restaurants" ("workingDays") VALUES ($1)`;
-    const res = await pool.query(query, [workingDays]);
-    return res.rows;
-  } catch (error) {
-    throw error;
-  }
-}
-
-async function getWorkingDays() {
-  try {
-    const query = `SELECT * FROM "RestaurantWorkingDays"`;
-    const res = await pool.query(query);
-    return res.rows;
-  } catch (error) {
-    throw error;
-  }
-}
-
-async function deleteWorkingDaysJunctions(restaurantId) {
-  try {
-    const query = `DELETE FROM "RestaurantWorkingDaysJunctions" WHERE "restaurantID" = $1`;
-    const res = await pool.query(query, [restaurantId]);
     return res.rows;
   } catch (error) {
     throw error;
@@ -66,9 +50,7 @@ async function changeRestaurantStatus(isActive, restaurantId) {
 
 module.exports = {
   setActiveHours,
+  updateActiveHours,
   deleteActiveHours,
-  setWorkingDays,
-  getWorkingDays,
-  deleteWorkingDaysJunctions,
   changeRestaurantStatus,
 };
