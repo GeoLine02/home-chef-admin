a238e7f1b1dba026d553f6f4cbe90ac262c253dc
Nika Wuladze <126957977+GeoLine02@users.noreply.github.com>
2024-06-05 23:44:14 +0400

    add: create updateActiveHours controller function

--- src/controllers/restaurant.settings.controller.js
+++ src/controllers/restaurant.settings.controller.js
@@ -1,62 +1,54 @@
-const restaurantService = require("../services/restaurant.settings.service");
+const restaurantSettingsService = require("../services/restaurant.settings.service");
 
 const setActiveHours = async (req, res) => {
   try {
-    const timeStemp = req.body.timeStemp;
-    timeStemp.map((data) => {
-      const hours = data.getHours();
-      const minutes = data.getMinutes();
-      return `${hours.toString().padStart(2, "0")}:${minutes.toString().padStart(2, "0")}`;
-    });
-    const [workingFrom, workingTill] = timeStemp;
-    await restaurantService.setActiveHours(workingFrom, workingTill);
+    const { workingFrom, workingTill } = req.body;
+    await restaurantSettingsService.setActiveHours(workingFrom, workingTill);
     res.status(200).json({ message: "active hours set successfully" });
   } catch (error) {
     throw error;
   }
 };
 
+const updateActiveHours = async (req, res) => {
+  try {
+    const restaurantID = req.params.id;
+    const { workingFrom, workingTill } = req.body;
+    if (!restaurantID) {
+      res
+        .status(400)
+        .json({ message: `restaurant with id ${restaurantID} does not exist` });
+    }
+
+    if (!workingFrom || !workingTill) {
+      res.status(400).json({ message: "working hours values are missing" });
+    }
+
+    if (workingFrom === workingTill) {
+      res.status(400).json({
+        message: "working hours values should not be equal to each other",
+      });
+    }
+    const updatedActiveHours = restaurantSettingsService.updateActiveHours(
+      restaurantID,
+      workingFrom,
+      workingTill
+    );
+    if (updatedActiveHours) {
+      res
+        .stat(201)
+        .json({ message: "restaurant active hours updated successfuly" });
+    }
+  } catch (error) {
+    res.status(500).send("internal server error");
+  }
+};
+
 const deleteActiveHours = async (req, res) => {
   try {
     const { restaurantId } = req.body;
-    await restaurantService.deleteActiveHours(restaurantId);
+    await restaurantSettingsService.deleteActiveHours(restaurantId);
     res.status(200).send("active hours deleted successfuly");
-  } catch (error) {
-    res.status(500).send("internal server error");
-    throw error;
-  }
-};
-
-const setWorkingDays = async (req, res) => {
-  try {
-    const wokringDays = req.body;
-    await restaurantService.setWorkingDays(wokringDays);
-    res.status(200).json({ message: "working days set successfully" });
-  } catch (error) {
-    throw error;
-  }
-};
-
-const getWorkingDays = async (req, res) => {
-  try {
-    const workingDays = await restaurantService.getWorkingDays();
-    res.status(200).json(workingDays);
-  } catch (error) {
-    throw error;
-  }
-};
-
-const deleteWorkingDaysJunctions = async (req, res) => {
-  try {
-    const { restaurantId } = req.body;
-    await restaurantService.deleteWorkingDaysJunctions(restaurantId);
-    if (restaurantId) {
-      res.status(200).send("working days jucntiosn deleted successfuly");
-    } else {
-      res
-        .status(404)
-        .send(`working days with restaurantID ${restaurantId} does not exist`);
-    }
   } catch (error) {
     res.status(500).send("internal server error");
     throw error;
@@ -68,7 +60,10 @@ const changeRestaurantStatus = async (req, res) => {
     const restaurantID = req.params.id;
 
     const { isActive } = req.body;
-    await restaurantService.changeRestaurantStatus(isActive, restaurantID);
+    await restaurantSettingsService.changeRestaurantStatus(
+      isActive,
+      restaurantID
+    );
     res.status(201).json({ message: "restaurant status changed succesfully" });
   } catch (error) {
     throw error;
@@ -77,9 +72,7 @@ const changeRestaurantStatus = async (req, res) => {
 
 module.exports = {
   setActiveHours,
+  updateActiveHours,
   deleteActiveHours,
-  setWorkingDays,
-  getWorkingDays,
-  deleteWorkingDaysJunctions,
   changeRestaurantStatus,
 };
